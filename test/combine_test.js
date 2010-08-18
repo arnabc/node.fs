b94a5da379c92b2463b8f1d2591cb76f8cad2882
Arnab Chakraborty <arnabc212@gmail.com>
2010-08-18 16:39:02 +0530

    Tweaks in MHTML hack generation, included Connect middleware to handle HTTP requests

--- test/combine_test.js
+++ test/combine_test.js
@@ -5,7 +5,7 @@ var sys = require('sys'),
     combiner = require('./../lib/combine'),
     global = require('./../lib/globals');
 
-var dirpath = resolvePublicURL( Config.CSS.ROOT );
+var dirpath = resolvePathToFilesystem( Config.CSS.ROOT );
 
 combiner.combine( dirpath, Config.CSS.PATTERN ).then( function ( arr ) {
     assert.equal( arr.length, 2, 'Total CSS files are 2, content returned ' + arr.length );   
--- test/data-uri_test.js
+++ test/data-uri_test.js
@@ -6,12 +6,12 @@ var sys = require('sys'),
     global = require('./../lib/globals');
 
 console.log( 'START:: Data URI generation Testing\n' );
-var p = resolvePublicURL( Config.CSS.ROOT + '/style.css' );
+var p = resolvePathToFilesystem( Config.CSS.ROOT + '/style.css' );
 
 console.log( 'CSS File to generate data:URIs ', p );
 
 dataUri.generate( p ).then( function ( content ) {
-    require('fs').writeFile( resolvePublicURL( Config.CSS.ROOT + '/' + Config.CSS.FILE ), content );
+    require('fs').writeFile( resolvePathToFilesystem( Config.CSS.ROOT + '/' + Config.CSS.FILE ), content );
     assert.ok( content.indexOf( 'data:' ) > -1, 'Unable to convert images to base64 encoded data:URIs');
 } );
 
